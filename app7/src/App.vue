9b2cc9622f4e62a91f89078910890ac66a8aa482
18476567885 <13727699@qq.com>
2021-02-18 15:49:20 +0800

    学习vue

---
+++ app7/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <!-- <HelloWorld :p="logPerson" v-for="(p1,index) in person" :key="index" :per="p1"/> -->
+   <todolis/>
+  </div>
+</template>
+
+<script>
+import todolis from './components/todolis.vue'
+
+export default {
+  name: 'App',
+  components: {
+    todolis
+  },
+  data(){
+    return{
+      
+    }
+  },
+  methods:{
+   
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app7/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+     <p>
+       <button @click="p">点我</button>
+       {{per.name}}
+       <button @click="lod">打印</button>
+     </p>
+  </div>
+</template>
+<script>
+  export default{
+    data(){
+      return{}
+    },
+    props:{
+      p:Function,
+      per:Object
+    },
+    methods:{
+  lod(){
+    console.log(this.per.age);
+  }
+    }
+  }
+</script>
